5272922e581fe012dc45b3b20cc8ef010e8580d8
nealichinohe <neal.kuni@yahoo.com>
2022-02-02 11:50:43 -0700

    footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -21,7 +21,7 @@ export default Footer;
 const FooterContainer = styled.footer`
     background: #7393B3;
     font-family: 'Poppins', sans-serif;
-    height: 5rem;
+    height: 2rem;
     left: 0;
     bottom: 0;
     width: 100%;
